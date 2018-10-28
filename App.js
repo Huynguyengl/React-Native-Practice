debfafc06870d80ef34207eb3bd587c5aa68eca7
Zekii <huynguyennbk@gmail.com>
2018-10-28 11:55:47 +0700

    Flex Change

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import {Image,Text,View} from 'react-native';
+import {Image,Text,View,StyleSheet} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     let text=`
 
-
-
-    Hello World
+    Naruto Love Sasuke
     `
     return(
-      <View style={{alignItems:"center"}}>
-        <Text>{text}</Text>
+      <View style={{flex:1,alignItems:"center"}}>
+      {/* Flex View */}
+       <View style={{flex:40,alignItems:"center"}}>
+        <Text style={styles.firstText}>{text}</Text>
+        <Image source={{uri:"https://dw9to29mmj727.cloudfront.net/properties/2016/525-SeriesThumb_NARSHP_400x320.jpg"}}
+              style={{width:300,height:200}}></Image>
+        </View>
+
+      {/* Flex View */}
+        <View style={{flex:60,alignItems:"center"}}>
+        <Text style={styles.firstText}>{text}</Text>
+        <Image source={{uri:"https://dw9to29mmj727.cloudfront.net/properties/2016/525-SeriesThumb_NARSHP_400x320.jpg"}}
+              style={{width:300,height:200}}></Image>
+        </View>
       </View>
     );
   }
 }
+const styles=StyleSheet.create({
+  firstText:{
+    margin:5,
+    color:'pink',
+    fontSize:24
+  }
+})
 
